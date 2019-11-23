35d76b148b1965f0062d9b1d66f7fe27671f8ee4
timmy <artem.chernenko1496@gmail.com>
2019-11-23 14:24:43 +0200

    added controller and first view

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('rest', 'RestTestController')->names('rest.test');
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
