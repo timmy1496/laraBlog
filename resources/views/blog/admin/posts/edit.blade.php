ec2b3efcbb7f21776573e131684f52f5ee5868fe
timmy <artem.chernenko1496@gmail.com>
2020-01-26 23:45:43 +0200

    complete create wiev

--- resources/views/blog/admin/posts/edit.blade.php
+++ resources/views/blog/admin/posts/edit.blade.php
@@ -21,4 +21,23 @@
                         @include('blog.admin.posts.includes.post_edit_add_col')
                     </div>
                 </div>
+            </form>
+            @if ($item->exists)
+            <br>
+            <form method="POST" action="{{ route('blog.admin.posts.destroy', $item->id) }}">
+                @method('DELETE')
+                @csrf
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card card-block">
+                            <div class="card-body ml-auto">
+                                <button type="submit" class="btn btn-link">Удалить</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+            </form>
+            @endif
     </div>
+@endsection
